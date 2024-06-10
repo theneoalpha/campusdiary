97b932f68d54f32a8bf7db52260e426eb857faaa
Vikash <vikashkaushik910@gmail.com>
2024-06-10 23:56:39 +0530

    Database changed

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const DB ="mongodb+srv://<username>:<password>@cluster0.lqezs6z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const DB ="mongodb+srv://theneoalpha:VikashKaushik@cluster0.lqezs6z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongoose.connect(DB).then(()=>{
     console.log("Connection successul");
 }).catch((err)=>{
