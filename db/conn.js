1d74a3a21e79e8aaed722c1671770a8cd944976c
Vikash <vikashkaushik910@gmail.com>
2024-06-10 21:30:01 +0530

    Database updated

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const DB ="mongodb+srv://theneoalpha:VikashKaushik@cluster0.l0jtluh.mongodb.net/?retryWrites=true&w=majority"
+const DB ="mongodb+srv://theneoalpha:VikashKaushik@cluster0.yfeu8x6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongoose.connect(DB).then(()=>{
     console.log("Connection successul");
 }).catch((err)=>{
