5d4c0862bca17ac1f74f1b565a7d27f465d3ab7a
Vikash <vikashkaushik910@gmail.com>
2024-06-18 09:20:02 +0530

    Mongo url tested

--- db/conn.js
+++ db/conn.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
-<<<<<<< HEAD
-const DB ='mongodb+srv://theneoalpha:VikashKaushik@cluster0.pydiecm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-=======
-const DB ="mongodb+srv://theneoalpha:VikashKaushik@cluster0.bfz9s5r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
->>>>>>> 142bbd0644062d6d750587af6d3fddbcaa7a32b5
+
+const DB ="mongodb+srv://Avinash:AvinashSingh@cluster0.fxhrtlq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 mongoose.connect(DB).then(()=>{
     console.log("Connection successul");
 }).catch((err)=>{
